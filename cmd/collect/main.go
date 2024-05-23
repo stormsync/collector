f0584b571a2da7974b5779b5c4383dad7bc8f2dd
jc <email@jasoncostello.com>
2024-05-22 19:02:57 -0500

    reworking config and introducing telemetry

--- cmd/collect/main.go
+++ cmd/collect/main.go
@@ -9,94 +9,96 @@ import (
 	"time"
 
 	slogenv "github.com/cbrewster/slog-env"
-	"github.com/redis/go-redis/v9"
-	"github.com/segmentio/kafka-go"
+	jaegerPropagator "go.opentelemetry.io/contrib/propagators/jaeger"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
 
 	"github.com/stormsync/collector"
-	"github.com/stormsync/collector/config"
+	"github.com/stormsync/collector/internal/config"
 )
 
 func main() {
+	// Create resource.
+
+	// const op = "api.banners.CreateBanner"
 	logger := slog.New(slogenv.NewHandler(slog.NewTextHandler(os.Stdout, nil)))
+	otel.SetTextMapPropagator(jaegerPropagator.Jaeger{})
+	ctx := context.Background()
+	traceProvider, err := startTracer()
+	if err != nil {
+		log.Fatal("Unable to initiate tracer: ", err)
+	}
+	defer func() {
+		if err := traceProvider.Shutdown(context.Background()); err != nil {
+			log.Fatalf("traceprovider: %v", err)
+		}
+	}()
 
-	environment := os.Getenv("ENVIRONMENT")
-
+	tracer := traceProvider.Tracer("collector")
+	ctx, span := tracer.Start(ctx, "main")
+	defer span.End()
+	startTracer()
 	vt := os.Getenv("VAULT_TOKEN")
 
-	config, err := config.NewConfig(environment)
+	config, err := config.NewConfig(ctx, vt)
 	if err != nil {
 		log.Fatal("unable to create new config: ", err)
 	}
-	config.FetchVaultData(vt)
-	var (
-		WindURL    = "https://www.spc.noaa.gov/climo/reports/today_wind.csv"
-		HailURL    = "https://www.spc.noaa.gov/climo/reports/today_hail.csv"
-		TornadoURL = "https://www.spc.noaa.gov/climo/reports/today_torn.csv"
-	)
 
 	interval, err := time.ParseDuration(config.Services.Collector.Interval)
 	if err != nil {
 		log.Fatal("failed to parse interval: ", err)
 	}
-	// GO_LOG=info;KAFKA_ADDRESS=organic-ray-9236-us1-kafka.upstash.io:9092;KAFKA_PASSWORD=ZGIwNWQwMDYtYjlhMC00NWI3LWFmN2QtMWZlMDVlNDQ0MjEw;KAFKA_USER=b3JnYW5pYy1yYXktOTIzNiQsp1hujJxnWrUY-WO8UK9CyCD7hcKBsa5731Ijvfk;POLLING_INTERVAL_IN_SECONDS=30;REDIS_ADDRESS=gentle-kingfish-45342.upstash.io;REDIS_DB_INT=0;REDIS_PASSWORD=AbEeAAIncDFjMzVlNjAxMTFiYTU0MTFlYWQ1NDk2YmM3NTQ2YmE3YnAxNDUzNDI;REDIS_USER=default;TOPIC=raw-weather-report
-	urlMap := map[collector.ReportType]string{
-		collector.Wind:    WindURL,
-		collector.Hail:    HailURL,
-		collector.Tornado: TornadoURL,
-	}
 
-	rcc := config.Services.Redis
-	rc, err := NewRedisClient(rcc.Host+":"+rcc.Port, rcc.User, rcc.Password, rcc.DB)
+	collector, err := collector.NewCollector(ctx, *config, tracer, logger)
 	if err != nil {
-		log.Fatal("failed to create redis client: ", err)
-	}
-
-	// kc := config.Services.Kafka
-	kw, err := NewKafkaWriter("kafka-1", "9202", "", "", "")
-	if err != nil {
-		log.Fatal("unable to create a kafka writer: ", err)
-	}
-
-	collector, err := collector.NewCollector(urlMap, rc, kw, logger)
-	if err != nil {
-		log.Fatal("failed to create the collect: %w", err)
+		log.Fatal("failed to create the collect: ", err)
 	}
 
 	logger.Info("Starting Collection Service", "collection interval", interval.String())
-	logger.Info("collecting source", "wind", WindURL)
-	logger.Info("collecting source", "hail", HailURL)
-	logger.Info("collecting source", "tornado", TornadoURL)
 
-	ctx := context.Background()
+	ctx = context.WithoutCancel(ctx)
 	collector.Poll(ctx, interval)
 }
 
-func NewKafkaWriter(host, port, topic, user, pw string) (*kafka.Writer, error) {
-	// mechanism, err := scram.Mechanism(scram.SHA256, "", "")
-	// if err != nil {
-	// 	return nil, fmt.Errorf("failed to create scram.Mechanism for auth: %w", err)
-	// }
-	w := &kafka.Writer{
-		Addr: kafka.TCP("kafka-1:9092"),
-		// Addr:  kafka.TCP(host + ":" + port),
-		Topic: "raw-weather-report",
-		// Transport: &kafka.Transport{
-		// 	SASL: mechanism,
-		// 	TLS:  &tls.Config{},
-		// },
+func startTracer() (*trace.TracerProvider, error) {
+	headers := map[string]string{
+		"content-type": "application/json",
 	}
-	return w, nil
-}
 
-func NewRedisClient(address, user, password string, db int) (*redis.Client, error) {
-	opt, err := redis.ParseURL(fmt.Sprintf("redis://%s:%s@%s", user, password, address))
+	exporter, err := otlptrace.New(
+		context.Background(),
+		otlptracehttp.NewClient(
+			otlptracehttp.WithEndpoint("localhost:4318"),
+			otlptracehttp.WithHeaders(headers),
+			otlptracehttp.WithInsecure(),
+		),
+	)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create new tracing exporter: %w", err)
 	}
-	opt.DB = 0
-	c := redis.NewClient(opt)
 
-	t := c.Time(context.Background())
-	fmt.Println("redis time: ", t.String())
-	return c, nil
+	tracerProvider := trace.NewTracerProvider(
+		trace.WithBatcher(
+			exporter,
+			trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
+			trace.WithBatchTimeout(trace.DefaultScheduleDelay*time.Millisecond),
+			trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
+		),
+		trace.WithResource(
+			resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String("collector"),
+			),
+		),
+	)
+
+	otel.SetTracerProvider(tracerProvider)
+
+	return tracerProvider, nil
+
 }
--- go.mod
+++ go.mod
@@ -4,31 +4,53 @@ go 1.22.2
 
 require (
 	github.com/cbrewster/slog-env v0.1.1
-	github.com/google/go-github/v62 v62.0.0
-	github.com/hashicorp/vault-client-go v0.4.3
+	github.com/hashicorp/vault/api v1.13.0
 	github.com/redis/go-redis/v9 v9.5.1
 	github.com/segmentio/kafka-go v0.4.47
+	go.opentelemetry.io/contrib/propagators/jaeger v1.27.0
+	go.opentelemetry.io/otel v1.27.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.27.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.27.0
+	go.opentelemetry.io/otel/metric v1.27.0
+	go.opentelemetry.io/otel/sdk v1.27.0
+	go.opentelemetry.io/otel/trace v1.27.0
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
+	github.com/cenkalti/backoff/v3 v3.0.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/go-jose/go-jose/v4 v4.0.1 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/hashicorp/go-retryablehttp v0.7.6 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
+	github.com/hashicorp/go-secure-stdlib/parseutil v0.1.6 // indirect
 	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/klauspost/compress v1.17.8 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pierrec/lz4/v4 v4.1.21 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
-	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
+	go.opentelemetry.io/proto/otlp v1.2.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240520151616-dc85e6b867a5 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240515191416-fc5f0ca64291 // indirect
+	google.golang.org/grpc v1.64.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
 )
